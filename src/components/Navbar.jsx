b5598efca3044b619bcb1cb95db5ffb9f6945924
Fahim047 <fahimulislam58@gmail.com>
2024-10-25 08:16:15 +0600

    💄 Navbar Design Done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+	return (
+		<header className="max-w-6xl mx-auto flex items-center justify-between py-4">
+			<img className="w-16" src="/assets/logo.png" alt="a batsman is playing" />
+			<nav className="flex items-center gap-12">
+				<ul className="text-gray-500 flex items-center gap-12">
+					<li>Home</li>
+					<li>Fixture</li>
+					<li>Teams</li>
+					<li>Schedules</li>
+				</ul>
+				<p className="border px-2 py-1 rounded min-w-24 text-center font-bold">
+					0 Coin 🪙
+				</p>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
